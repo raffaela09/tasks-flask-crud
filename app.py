8d2066b9e0503944f1948931c3dbf238950b0423
raffaela09 <rafaelafranco096@gmail.com>
2025-01-27 18:35:06 -0400

    criacao do create_taks

--- app.py
+++ app.py
@@ -1,31 +1,26 @@
-from flask import Flask, render_template
-
-#variave name -> __name__ = '__main__' forma manual 
+from flask import Flask, request, jsonify
+from models.task import Task
+ 
 app = Flask(__name__)
 
-#rota para criar um comunicação
-@app.route('/') #rota inicial - 
-#o que vai ser executado a partir dessa função
-def hello_world():
-    return 'Hello world!'
 
-@app.route('/about') #endpoints 
-def about():
-    return render_template('about.html') #chama uma pagina html 
+#CRUD   
+#Create, Read, Update e Delete
+#Tabela: Tarefa
+
+tasks = []
+task_id_control = 1
+@app.route('/tasks',methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json() #recupera o que o cliente enviou
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get('description'))
+    task_id_control +=1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({'message': 'Nova tarefa criada com sucesso!'})
 
 if __name__ == '__main__':
-    #rodar o codigo, desenvolvimento local, executa somente se for rodado de forma manual
-    app.run(debug=True) #coleta as informações de log
+
+    app.run(debug=True) 
     
-#api - aplication programming interface - porta de entrada (interface)
-#api rest - representational state transfer - conjunto de regras para que a comunicação seja de forma fluida - HTTP
-#json ou xml - formato de devolver a url pro client 
-#api rest e api restful - ao respeitar todos os padroes do rest - ela é restful - estilo de arquitetura para construção de api
-#get - apenas para retornar
-#post - para inserir informações em um recurso especifico, problema para alterar
-#put - atualizações - ele substitui todas as informações atuais desse cliente, sendo obrigada a enviar tudo
-#patch - modificações parciais
-#delete - remove recurso especifico 
-#request e response  - response vem aquisi
-#documentação de api - swagger.io - usado para entender oq aquela api faz
-#postman - para testar apis
